0b1ced8542b63120c10ee193ab5c840de946e4e8
siyam79 <siyamahmed3827@gmail.com>
2023-10-19 16:25:58 +0600

    node_modules be gone!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -9,11 +10,10 @@ const port = process.env.PORT || 5000;
 //   middle ware
 app.use(cors());
 app.use(express.json());
-//  siyamahmed3827
-// 4c7AvUR0MNbpQ3Hb
 
 
-const uri = "mongodb+srv://siyamahmed3827:4c7AvUR0MNbpQ3Hb@cluster0.6784glr.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6784glr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,21 +31,26 @@ async function run() {
 
         const productCollection = client.db("productDB").collection("products");
 
-             app.post('/products', async(req , res ) =>{
-                const product = req.body;
-                console.log(product)
-                const result = await productCollection.insertOne(product);
-                res.send(result)
-        
-             })
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            console.log(product)
+            const result = await productCollection.insertOne(product);
+            res.send(result)
 
+        })
 
+        app.get("/products", async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
-             app.get("/products" , async(req , res ) =>{
-                const cursor = productCollection.find();
-                const result = await cursor.toArray()
-                res.send(result)
-             })
+        app.get("/brands" , async(req , res )=>{
+            const cursor = productCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
 
 
 
